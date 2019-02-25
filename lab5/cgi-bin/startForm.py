0fa632e82bc0e1652fe7618326b0db605b1a7d65
kostya05983 <kostya05983@mail.ru>
2019-02-26 00:11:54 +0700

    add forms

--- lab5/cgi-bin/startForm.py
+++ lab5/cgi-bin/startForm.py
@@ -1,4 +1,35 @@
+#!/usr/bin/env python3
+
+head = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>first Question</title>
+</head>
+<body>
+"""
+
+first_question = """
+ <p>В каком случае водитель совершит вынужденную остановку?</p>
+    <form id="choose" action="/cgi-bin/question.py" method="%s" target="_blank"
+      style="align-self: center; text-align: center">
+    <input type="radio" name="question" value="on"/> Остановившись непосредственно перед пешеходным переходом, чтобы уступить дорогу пешеходу
+    <input type="radio" name="question" value="off"/> Остановившись на проезжей части из-за технической неисправности транспортного средства
+    <input type="radio" name="question" value="off"/> В обоих перечисленных случаях
+    <input type="submit" value="Ок"/>
+</form>
+"""
+
 import cgi, cgitb
 
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
+
+method = form.getvalue("form")
+
+if form.getvalue("form"):
+    print(head)
+    print(first_question % "GET")
+else:
+    print(first_question % "POST")
