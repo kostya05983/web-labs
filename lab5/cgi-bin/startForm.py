8891791d34224b0cc275cea30c129bad7d9c2c68
kostya05983 <kostya05983@mail.ru>
2019-03-03 23:55:47 +0700

    Made common Scheme

--- lab5/cgi-bin/startForm.py
+++ lab5/cgi-bin/startForm.py
@@ -29,7 +29,7 @@ form_end = """
 """
 
 common_question = """
-    <input type="radio" name="question" value="1"/> %s
+    <input type="radio" name="question" value="%d"/> %s
     <br>
 """
 
@@ -43,24 +43,48 @@ def increment_current():
     else:
         storage.set_cookie_result(cook["session"].value, "current", 0)
 
+
 def print_form(method, current):
     print(form_start % (questions[current][0], method))
+    i = 1
     for value in questions[current][1]:
-        print(common_question % value)
+        print(common_question % (i, value))
+        i = i + 1
     print(form_end)
+
+
+def right_answer():
+    current = storage.find_cookie_session(cook["session"].value, "current")
+    value = form.getvalue("question")
+    res = storage.find_cookie_session(cook["session"].value, "result")
+    if (res == 0):
+        res = 0
+    if int(questions[current - 1][2]) == value:
+        res = storage.find_cookie_session(cook["session"].value, "result")
+        storage.set_cookie_result(cook["session"].value, "result", int(res) + 1)
+    print("Set-cookie: result=" + res)
+
+
+def if_last():
+    current = storage.find_cookie_session(cook["session"].value, "current")
+    print("Content-type: text/html\n")
+    print(head)
+    if current == len(questions):
+        print("Uryaaa")
+        storage.set_cookie_result(cook["session"].value, "current", 0)
+    else:
+        method = "GET"
+        if current == 0:
+            method = form.getvalue("method")
+        print(storage.find_cookie_session(cook["session"].value, "current"))
+        print_form(method, storage.find_cookie_session(cook["session"].value, "current"))
+        if current != 0:
+            right_answer()
+        increment_current()
 
 
 storage = CookStorage()
 cook = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
-# Create instance of FieldStorage
 form = cgi.FieldStorage()
-
-value = form.getvalue("question")
-
-increment_current()
-print("Content-type: text/html\n")
-print(head)
-print(value)
-print(storage.find_cookie_session(cook["session"].value, "current"))
-print_form("GET", storage.find_cookie_session(cook["session"].value, "current"))
-print()
+if_last()
+# Create instance of FieldStorage
